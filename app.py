9598245d046275342e93f336224421b26c891929
RaniaFleifel <raniatarekfleifel@gmail.com>
2023-08-08 23:46:38 +0300

    Update app.py first insert

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,3 +6,33 @@ app = Flask(__name__)
 #    return 'Hello, World!'
 def home():
     return render_template('index_new_v2.html')
+
+@app.route('/amount',methods=['POST'])
+
+def amount():
+    #a=[]
+    loan_size=[int(x) for x in request.form.values()]#int(request.form.values())
+    while loan_size[0]>15000:        
+        tmp="The cap of the loan is 15000.. Please re-enter the loan amount"
+        # loan_size=[int(x) for x in request.form.values() if isinstance(x,int)]#int(request.form.values())
+        for x in request.form.values():
+            if isinstance(x,int):
+                loan_size=int(x)
+            else:
+                continue
+        output=loan_size#np.round_(prediction[0],2)
+        
+        return render_template('index_new_v2.html',loan_size_txt=tmp)#.append('The size of the loan is {} egp\n'.format(loan_size[0])))
+
+    else:
+        print(request.form.values())
+        tmp=f"You selected: {loan_size[0]} egp"
+        output=loan_size
+        #print(output,output[0])
+        print("new entry###############################################")
+        
+        a.append(loan_size[0])
+        print(a)
+
+        return render_template('index_new_v2.html',loan_size_txt=tmp)
+    
