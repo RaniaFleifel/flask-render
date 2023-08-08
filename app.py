a3f8e8b48cda25541510527152129541c618824f
RaniaFleifel <raniatarekfleifel@gmail.com>
2023-08-08 23:53:47 +0300

    Update app.py -freq

--- app.py
+++ app.py
@@ -37,3 +37,31 @@ def amount():
 
         return render_template('index_new_v2.html',loan_size_txt=tmp)
     
+@app.route('/freq',methods=['POST'])
+def freq():     
+        print("freq ###############################################")
+        print(len(a)-2)
+
+        if len(a)<=4:
+            loan_size_txt="Loan amount= {} egp".format(a[0])
+        else:
+            loan_size_txt="Loan amount= {} egp".format(a[len(a)-1])
+        # Get the user-selected value from the dropdown
+        selected_option = request.form['option']
+        a.append(selected_option)
+        
+        # Process the selected option (You can add your own logic here)
+        result = f"You selected: {selected_option} payements"
+        tom_date = datetime.now()+timedelta(1)
+        tom_month=tom_date.strftime("%b")
+        
+        print(tom_month)
+       
+        if selected_option=='monthly':
+            result+="..... Dispersment starts tomorrow (aka month {})".format(tom_month)
+            a.append(tom_month)
+        else:
+            result+="..This option is not applied in the website yet"  
+            
+        print(a)
+        return render_template('index_new_v2.html',loan_size_txt=loan_size_txt,frequency_txt=result)#tmp)#+"<br/>The provided loan size is {} EGP".format(output[0]))#.append('The size of the loan is {} egp\n'.format(loan_size[0])))
