a154b5fb90438d73de5604d2184c833434067e8f
RaniaFleifel <raniatarekfleifel@gmail.com>
2023-08-08 23:33:22 +0300

    Update app.py to include home

--- app.py
+++ app.py
@@ -2,5 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+#def hello_world():
+#    return 'Hello, World!'
+def home():
+    return render_template('index_new_v2.html')
